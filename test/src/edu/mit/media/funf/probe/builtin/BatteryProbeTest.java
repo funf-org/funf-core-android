721778d05f7ed9535d8c7a7e60e1914131cfc05b
Alan Gardner <alan.gardner@gmail.com>
2011-09-15 15:30:48 -0700

    Added license to the top of all source files: java, xml, and python.

--- test/src/edu/mit/media/funf/probe/builtin/BatteryProbeTest.java
+++ test/src/edu/mit/media/funf/probe/builtin/BatteryProbeTest.java
@@ -1,21 +1,25 @@
 /**
- *
- * This file is part of the FunF Software System
- * Copyright © 2011, Massachusetts Institute of Technology
- * Do not distribute or use without explicit permission.
- * Contact: funf.mit.edu
- *
- *
+ * Funf: Open Sensing Framework
+ * Copyright (C) 2010-2011 Nadav Aharony, Wei Pan, Alex Pentland. 
+ * Acknowledgments: Alan Gardner
+ * Contact: nadav@media.mit.edu
+ * 
+ * This file is part of Funf.
+ * 
+ * Funf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ * 
+ * Funf is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with Funf. If not, see <http://www.gnu.org/licenses/>.
  */
-package edu.mit.media.funf.probe.builtin;
-
-import android.os.BatteryManager;
-import android.os.Bundle;
-import android.util.Log;
-
-public class BatteryProbeTest extends ProbeTestCase<BatteryProbe> {
-
-	public BatteryProbeTest() {
+atteryProbeTest() {
 		super(BatteryProbe.class);
 	}
 	
