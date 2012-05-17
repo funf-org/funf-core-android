8972664015830103ef921c38aa163f5a5add1d8e
Alan Gardner <alan.gardner@gmail.com>
2012-05-17 11:55:02 -0700

    Added java license headers.

--- src/edu/mit/media/funf/security/RSAEncode.java
+++ src/edu/mit/media/funf/security/RSAEncode.java
@@ -1,36 +1,28 @@
 /**
+ * 
  * Funf: Open Sensing Framework
- * Copyright (C) 2010-2011 Nadav Aharony, Wei Pan, Alex Pentland. 
+ * Copyright (C) 2010-2011 Nadav Aharony, Wei Pan, Alex Pentland.
  * Acknowledgments: Alan Gardner
  * Contact: nadav@media.mit.edu
  * 
  * This file is part of Funf.
  * 
  * Funf is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of 
- * the License, or (at your option) any later version. 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  * 
- * Funf is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * Funf is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public 
+ * You should have received a copy of the GNU Lesser General Public
  * License along with Funf. If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
 // using /res/raw/pub.enc as the RSA Public Key
 package edu.mit.media.funf.security;
-/**
- * 
- * This file is part of the FunF Software System
- * Copyright � 2010, Massachusetts Institute of Technology  
- * Do not distribute or use without permission.
- * Contact: Nadav Aharony (nadav@mit.edu) or friendsandfamily@media.mit.edu
- * 
- * @date Feb, 2010
- * 
- */
 
 import java.util.ArrayList;
 
