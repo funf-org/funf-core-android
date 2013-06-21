5a3bc6bb2458ee11dc1c94711b1b7603a1bdcfbd
Pararth Shah <pararthshah717@gmail.com>
2013-06-21 21:01:04 +0530

    Added AudioRecorderProbe, for basic audio recording

--- src/edu/mit/media/funf/probe/builtin/ProbeKeys.java
+++ src/edu/mit/media/funf/probe/builtin/ProbeKeys.java
@@ -192,6 +192,11 @@ public class ProbeKeys {
 		DEVICE_ID = "deviceId";	
 	}
 	
+	public static interface HighBandwidthKeys extends BaseProbeKeys {
+		public static final String 
+		FILENAME = "filename";
+	}
+	
 	public static interface ImagesKeys extends BaseProbeKeys, android.provider.MediaStore.Images.ImageColumns {
 		public static final String 
 		IMAGES = "images";	
